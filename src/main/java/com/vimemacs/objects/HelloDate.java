df8ee504d0dbb76c131ee1c0fcb2a104849cec2f
VimIcewind <VimIcewind@gmail.com>
2021-11-17 10:00:43 +0800

    HelloDate

---
+++ src/main/java/com/vimemacs/objects/HelloDate.java
@@ -0,0 +1,14 @@
+package com.vimemacs.objects;
+
+import java.util.*;
+
+/**
+ * @author HWD
+ * @date 2021/11/17 09:54
+ */
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
