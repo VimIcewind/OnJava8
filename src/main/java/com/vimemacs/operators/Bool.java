dfe367e45b536edaff31d08b93fa9679af754ca3
VimIcewind <VimIcewind@gmail.com>
2022-12-21 11:49:58 +0800

    Bool

---
+++ src/main/java/com/vimemacs/operators/Bool.java
@@ -0,0 +1,29 @@
+package com.vimemacs.operators;
+
+import java.util.Random;
+
+/**
+ * @author HWD
+ * @date 2022/12/21 11:08
+ */
+public class Bool {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        int i = rand.nextInt(100);
+        int j = rand.nextInt(100);
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("i > j is " + (i > j));
+        System.out.println("i < j is " + (i < j));
+        System.out.println("i >= j is " + (i >= j));
+        System.out.println("i <= j is " + (i <= j));
+        System.out.println("i == j is " + (i == j));
+        System.out.println("i != j is " + (i != j));
+        // Treating an int as a boolean is not legal Java:
+        //- System.out.println("i && j is " + (i && j));
+        //- System.out.println("i || j is " + (i || j));
+        //- System.out.println("!i is " + !i);
+        System.out.println("(i < 10) && (j < 10) is " + ((i < 10) && (j < 10)));
+        System.out.println("(i < 10) || (j < 10) is " + ((i < 10) || (j < 10)));
+    }
+}
