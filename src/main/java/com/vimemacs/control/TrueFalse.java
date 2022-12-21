69c2ee84aec8dcc0e295f3204e5860365f7eb29c
VimIcewind <VimIcewind@gmail.com>
2022-12-21 17:15:13 +0800

    TrueFalse

---
+++ src/main/java/com/vimemacs/control/TrueFalse.java
@@ -0,0 +1,12 @@
+package com.vimemacs.control;
+
+/**
+ * @author HWD
+ * @date 2022/12/21 17:11
+ */
+public class TrueFalse {
+    public static void main(String[] args) {
+        System.out.println(1 == 1);
+        System.out.println(1 == 2);
+    }
+}
