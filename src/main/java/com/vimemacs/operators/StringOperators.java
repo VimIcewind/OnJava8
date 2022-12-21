77246f42db76a38e5b474b126e02e5927e001c8d
VimIcewind <VimIcewind@gmail.com>
2022-12-21 15:59:22 +0800

    StringOperators

---
+++ src/main/java/com/vimemacs/operators/StringOperators.java
@@ -0,0 +1,20 @@
+package com.vimemacs.operators;
+
+/**
+ * @author HWD
+ * @date 2022/12/21 15:54
+ */
+public class StringOperators {
+    public static void main(String[] args) {
+        int x = 0, y = 1, z = 2;
+        String s = "x, y, z ";
+        System.out.println(s + x + y + z);
+        // 将 x 转换为字符串
+        System.out.println(x + " " + s);
+        s += "(summed) = ";
+        // 级联操作
+        System.out.println(s + (x + y + z));
+        // Integer.toString() 方法的简写：
+        System.out.println("" + x);
+    }
+}
