0a2d816cdc3d7179d3054399d96c2e53476b86b5
VimIcewind <VimIcewind@gmail.com>
2022-12-21 10:41:14 +0800

    Equivalence

---
+++ src/main/java/com/vimemacs/operators/Equivalence.java
@@ -0,0 +1,14 @@
+package com.vimemacs.operators;
+
+/**
+ * @author HWD
+ * @date 2022/12/21 10:37
+ */
+public class Equivalence {
+    public static void main(String[] args) {
+        Integer n1 = 47;
+        Integer n2 = 47;
+        System.out.println(n1 == n2);
+        System.out.println(n1 != n2);
+    }
+}
