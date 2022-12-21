75bc458fbdc89e36c063027a6f37ed82560ef7cf
VimIcewind <VimIcewind@gmail.com>
2022-12-21 10:50:49 +0800

    EqualsMethod

---
+++ src/main/java/com/vimemacs/operators/EqualsMethod.java
@@ -0,0 +1,13 @@
+package com.vimemacs.operators;
+
+/**
+ * @author HWD
+ * @date 2022/12/21 10:48
+ */
+public class EqualsMethod {
+    public static void main(String[] args) {
+        Integer n1 = 47;
+        Integer n2 = 47;
+        System.out.println(n1.equals(n2));
+    }
+}
