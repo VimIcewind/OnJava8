49fc651b654842d2f17ebb741cfe97f5241aa7d8
VimIcewind <VimIcewind@gmail.com>
2022-12-21 22:07:23 +0800

    ForInString

---
+++ src/main/java/com/vimemacs/control/ForInSting.java
@@ -0,0 +1,14 @@
+package com.vimemacs.control;
+
+import java.util.Locale;
+
+/**
+ * @author HWD
+ * @date 2022/12/21 22:05
+ */
+public class ForInSting {
+    public static void main(String[] args) {
+        for (char c : "An African Swallow".toCharArray())
+            System.out.print(c + " ");
+    }
+}
