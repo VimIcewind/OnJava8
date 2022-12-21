3ba471dba1c107f99b2ed128f461597c5a07c513
VimIcewind <VimIcewind@gmail.com>
2022-12-21 10:33:05 +0800

    AutoInc

---
+++ src/main/java/com/vimemacs/operators/AutoInc.java
@@ -0,0 +1,18 @@
+package com.vimemacs.operators;
+
+/**
+ * @author HWD
+ * @date 2022/12/21 10:30
+ */
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i: " + i);
+        System.out.println("++i: " + ++i);
+        System.out.println("i++: " + i++);
+        System.out.println("i: " + i);
+        System.out.println("--i: " + --i);
+        System.out.println("i--: " + i--);
+        System.out.println("i: " + i);
+    }
+}
