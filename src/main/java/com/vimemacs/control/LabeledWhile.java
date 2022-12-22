8078447ec6b5ee8a39fd1f1dc8247dc9263d6d45
VimIcewind <VimIcewind@gmail.com>
2022-12-22 10:49:54 +0800

    LabeledWhile

---
+++ src/main/java/com/vimemacs/control/LabeledWhile.java
@@ -0,0 +1,35 @@
+package com.vimemacs.control;
+
+/**
+ * @author HWD
+ * @date 2022/12/22 10:47
+ */
+public class LabeledWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        outer:
+        while (true) {
+            System.out.println("Outer while loop");
+            while (true) {
+                i++;
+                System.out.println("i = " + i);
+                if (i == 1) {
+                    System.out.println("continue");
+                    continue;
+                }
+                if (i == 3) {
+                    System.out.println("continue outer");
+                    continue outer;
+                }
+                if (i == 5) {
+                    System.out.println("break");
+                    break;
+                }
+                if (i == 7) {
+                    System.out.println("break outer");
+                    break outer;
+                }
+            }
+        }
+    }
+}
