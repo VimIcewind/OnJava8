ce3cbc3c326dab4c0bd8234a1de2e8011811f1cc
VimIcewind <VimIcewind@gmail.com>
2022-12-23 09:41:11 +0800

    OverloadingOrder

---
+++ src/main/java/com/vimemacs/housekeeping/OverloadingOrder.java
@@ -0,0 +1,20 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 09:38
+ */
+public class OverloadingOrder {
+    static void f(String s, int i) {
+        System.out.println("String: " + s + ", int: " + i);
+    }
+
+    static void f(int i, String s) {
+        System.out.println("int: " + i + ", String: " + s);
+    }
+
+    public static void main(String[] args) {
+        f("String first", 1);
+        f(99, "Int first");
+    }
+}
