261ba18b3c75349daa9ae8d89f4a971ad72e0456
VimIcewind <VimIcewind@gmail.com>
2022-12-23 14:55:56 +0800

    TerminationCondition

---
+++ src/main/java/com/vimemacs/housekeeping/TerminationCondition.java
@@ -0,0 +1,41 @@
+package com.vimemacs.housekeeping;
+
+import onjava.*;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 11:22
+ */
+class Book {
+    boolean checkedOut = false;
+
+    Book(boolean checkOut) {
+        checkedOut = checkOut;
+    }
+
+    void checkIn() {
+        checkedOut = false;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        if (checkedOut) {
+            System.out.println("Error: checked out");
+        }
+        // Normally, you'll also do this:
+        // super.finalize(); // Call the base-class version
+    }
+}
+
+public class TerminationCondition {
+    public static void main(String[] args) {
+        Book novel = new Book(true);
+        //  Proper cleanup
+        novel.checkIn();
+        // Drop the reference, forget to clean up:
+        new Book(true);
+        // Force garbage collection & finalization:
+        System.gc();
+        new Nap(1); // one second delay
+    }
+}
