72b71be92b23a03acde784b8eca7f1c111271458
VimIcewind <VimIcewind@gmail.com>
2022-12-23 10:22:13 +0800

    Apricot

---
+++ src/main/java/com/vimemacs/housekeeping/Apricot.java
@@ -0,0 +1,16 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 10:21
+ */
+public class Apricot {
+    void pick() {
+        /* ... */
+    }
+
+    void pit() {
+        pick();
+        /* ... */
+    }
+}
