f48656abde066e05f104a7d8de20eea8454123e5
VimIcewind <VimIcewind@gmail.com>
2022-12-23 21:30:43 +0800

    MethodInit

---
+++ src/main/java/com/vimemacs/housekeeping/MethodInit.java
@@ -0,0 +1,13 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 21:30
+ */
+public class MethodInit {
+    int i = f();
+
+    int f() {
+        return 11;
+    }
+}
