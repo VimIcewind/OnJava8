3fac1f8cf5a176c60a74c905b049c94730ecbca9
VimIcewind <VimIcewind@gmail.com>
2022-12-23 10:11:38 +0800

    DefaultConstructor

---
+++ src/main/java/com/vimemacs/housekeeping/DefaultConstructor.java
@@ -0,0 +1,13 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 10:10
+ */
+class Bird {}
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+    }
+}
