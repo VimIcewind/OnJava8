638ff0884e9a6eaa19e2d389bf6c46dd749497cf
VimIcewind <VimIcewind@gmail.com>
2022-12-23 21:26:18 +0800

    InitialValues2

---
+++ src/main/java/com/vimemacs/housekeeping/InitialValues2.java
@@ -0,0 +1,16 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 21:24
+ */
+public class InitialValues2 {
+    boolean t = true;
+    char ch = 'x';
+    byte b = 47;
+    short s = 0xff;
+    int i = 999;
+    long lng = 1;
+    float f = 3.14f;
+    double d = 3.14159;
+}
