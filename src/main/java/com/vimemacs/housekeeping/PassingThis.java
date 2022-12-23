7350da57d53f23c03a2b02fcb7883777dccc1a78
VimIcewind <VimIcewind@gmail.com>
2022-12-23 10:31:08 +0800

    PassingThis

---
+++ src/main/java/com/vimemacs/housekeeping/PassingThis.java
@@ -0,0 +1,31 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 10:25
+ */
+class Person {
+    public void eat(Apple apple) {
+        Apple peeled = apple.getPeeled();
+        System.out.println("Yummy");
+    }
+}
+
+class Peeler {
+    static Apple peel(Apple apple) {
+        // ... remove peel
+        return apple; // Peeled
+    }
+}
+
+class Apple {
+    Apple getPeeled() {
+        return Peeler.peel(this);
+    }
+}
+
+public class PassingThis {
+    public static void main(String[] args) {
+        new Person().eat(new Apple());
+    }
+}
