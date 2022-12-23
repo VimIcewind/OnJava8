ed070dd19515a38badda6af97e6de21e91a37aa0
VimIcewind <VimIcewind@gmail.com>
2022-12-23 10:20:38 +0800

    BananaPeel

---
+++ src/main/java/com/vimemacs/housekeeping/BananaPeel.java
@@ -0,0 +1,19 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 10:18
+ */
+class Banana {
+    void peel(int i) {
+        /*...*/
+    }
+}
+
+public class BananaPeel {
+    public static void main(String[] args) {
+        Banana a = new Banana(), b = new Banana();
+        a.peel(1);
+        b.peel(2);
+    }
+}
