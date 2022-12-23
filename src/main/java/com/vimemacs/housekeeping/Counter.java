cee21f41aa5b696eb7fa42a3fafb7cd0ee4a22fc
VimIcewind <VimIcewind@gmail.com>
2022-12-23 21:50:09 +0800

    Counter

---
+++ src/main/java/com/vimemacs/housekeeping/Counter.java
@@ -0,0 +1,14 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2022/12/23 21:48
+ */
+public class Counter {
+    int i;
+
+    Counter() {
+        i = 7;
+    }
+    // ...
+}
