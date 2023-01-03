bca933249765e58f100f3291f5ea9c8ab6eb9b7b
VimIcewind <VimIcewind@gmail.com>
2023-01-03 12:51:07 +0800

    ArraysOfPrimitives

---
+++ src/main/java/com/vimemacs/housekeeping/ArraysOfPrimitives.java
@@ -0,0 +1,19 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2023/1/3 12:47
+ */
+public class ArraysOfPrimitives {
+    public static void main(String[] args) {
+        int[] a1 = {1, 2, 3, 4, 5};
+        int[] a2;
+        a2 = a1;
+        for (int i = 0; i < a2.length; i++) {
+            a2[i] += 1;
+        }
+        for (int i = 0; i < a1.length; i++) {
+            System.out.println("a1[" + i + "] = " + a1[i]);
+        }
+    }
+}
