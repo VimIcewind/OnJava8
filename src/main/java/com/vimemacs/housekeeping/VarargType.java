55adbe8f81b69cb7be4e896e18286bba737d192a
VimIcewind <VimIcewind@gmail.com>
2023-01-03 14:10:43 +0800

    VarargType

---
+++ src/main/java/com/vimemacs/housekeeping/VarargType.java
@@ -0,0 +1,25 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * @author HWD
+ * @date 2023/1/3 14:05
+ */
+public class VarargType {
+    static void f(Character... args) {
+        System.out.print(args.getClass());
+        System.out.println(" length " + args.length);
+    }
+
+    static void g(int... args) {
+        System.out.print(args.getClass());
+        System.out.println(" length " + args.length);
+    }
+
+    public static void main(String[] args) {
+        f('a');
+        f();
+        g(1);
+        g();
+        System.out.println("int[]: " + new int[0].getClass());
+    }
+}
