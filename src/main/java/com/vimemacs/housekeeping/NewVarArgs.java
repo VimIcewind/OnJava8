c39fd3072e21ad74315978e456baa2541fbdf273
VimIcewind <VimIcewind@gmail.com>
2023-01-03 14:02:37 +0800

    NewVarArgs

---
+++ src/main/java/com/vimemacs/housekeeping/NewVarArgs.java
@@ -0,0 +1,28 @@
+package com.vimemacs.housekeeping;
+
+/**
+ * Using array syntax to create variable argument lists
+ *
+ * @author HWD
+ * @date 2023/1/3 13:46
+ */
+
+public class NewVarArgs {
+    static void printArray(Object... args) {
+        for (Object obj : args) {
+            System.out.print(obj + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Can take individual elements:
+        printArray(47, (float) 3.14, 11.11);
+        printArray(47, 3.14F, 11.11);
+        printArray("one", "two", "three");
+        printArray(new A(), new A(), new A());
+        // Or an array:
+        printArray((Object[]) new Integer[] {1, 2, 3, 4,});
+        printArray();
+    }
+}
