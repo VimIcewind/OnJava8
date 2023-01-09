684ce753bcd3146b4a50b19c0970724fb9643d05
VimIcewind <VimIcewind@gmail.com>
2023-01-09 22:11:55 +0800

    SingleImport

---
+++ src/main/java/com/vimemacs/hiding/FullQualification.java
@@ -0,0 +1,11 @@
+package com.vimemacs.hiding;
+
+/**
+ * @author HWD
+ * @date 2023/1/3 15:42
+ */
+public class FullQualification {
+    public static void main(String[] args) {
+        java.util.ArrayList list = new java.util.ArrayList();
+    }
+}
---
+++ src/main/java/com/vimemacs/hiding/SingleImport.java
@@ -0,0 +1,13 @@
+package com.vimemacs.hiding;
+
+import java.util.ArrayList;
+
+/**
+ * @author HWD
+ * @date 2023/1/3 15:43
+ */
+public class SingleImport {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+    }
+}
