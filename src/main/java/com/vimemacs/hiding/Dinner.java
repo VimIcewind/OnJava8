a85deaa53b573116334f8ae29080698495fa9db7
VimIcewind <VimIcewind@gmail.com>
2023-01-24 20:51:25 +0800

    package or default access

---
+++ src/main/java/com/vimemacs/hiding/Dinner.java
@@ -0,0 +1,15 @@
+package com.vimemacs.hiding;
+
+import com.vimemacs.hiding.dessert.Cookie;
+
+/**
+ * Use the library
+ * @author HWD
+ * @date 2023/1/24 20:42
+ */
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        //- x.bite(); // Can's access
+    }
+}
---
+++ src/main/java/com/vimemacs/hiding/dessert/Cookie.java
@@ -0,0 +1,16 @@
+package com.vimemacs.hiding.dessert;
+
+/**
+ * Create a library
+ * @author HWD
+ * @date 2023/1/24 20:40
+ */
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+
+    void bite() {
+        System.out.println("bite");
+    }
+}
