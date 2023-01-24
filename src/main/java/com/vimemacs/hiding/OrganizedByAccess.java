f4e7de1ae487413c952b0739d7883cd936abafd8
VimIcewind <VimIcewind@gmail.com>
2023-01-24 22:20:56 +0800

    Interface and Implementation

---
+++ src/main/java/com/vimemacs/hiding/OrganizedByAccess.java
@@ -0,0 +1,15 @@
+package com.vimemacs.hiding;
+
+/**
+ * @author HWD
+ * @date 2023/1/24 22:17
+ */
+public class OrganizedByAccess {
+    public void pub1() {/*  ... */}
+    public void pub2() {/*  ... */}
+    public void pub3() {/*  ... */}
+    private void priv1() {/* ... */}
+    private void priv2() {/* ... */}
+    private void priv3() {/* ... */}
+    private int i;
+}
