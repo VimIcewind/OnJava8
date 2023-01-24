b01300a2599e54f05207464aa779f16995fc25f3
VimIcewind <VimIcewind@gmail.com>
2023-01-24 20:57:54 +0800

    Same directory but without package statement

---
+++ src/main/java/com/vimemacs/hiding/Cake.java
@@ -0,0 +1,11 @@
+/**
+ * Accesses a class in a separate compilation unit
+ * @author HWD
+ * @date 2023/1/24 20:52
+ */
+public class Cake {
+    public static void main(String[] args) {
+        Pie x = new Pie();
+        x.f();
+    }
+}
---
+++ src/main/java/com/vimemacs/hiding/Pie.java
@@ -0,0 +1,10 @@
+/**
+ * The other class
+ * @author HWD
+ * @date 2023/1/24 20:53
+ */
+public class Pie {
+    void f() {
+        System.out.println("Pie.f()");
+    }
+}
