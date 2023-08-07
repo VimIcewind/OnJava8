8b26c1f9b53a00016c070e97a6433e931e3e17e0
VimIcewind <VimIcewind@gmail.com>
2023-08-07 16:12:57 +0800

    Override

---
+++ src/main/java/com/vimemacs/reuse/Lisa.java
@@ -0,0 +1,13 @@
+package com.vimemacs.reuse;
+
+/**
+ * @author HWD
+ * @date 2023/8/7 16:07
+ */
+// {WillNotCompile}
+public class Lisa extends Homer {
+    // @Override
+    void doh(Milhouse m) {
+        System.out.println("doh(Milhouse)");
+    }
+}
