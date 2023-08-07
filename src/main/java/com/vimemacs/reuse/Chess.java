98567b91db1e5ddc8b46e5076f166233548e0cd9
VimIcewind <VimIcewind@gmail.com>
2023-08-07 14:34:49 +0800

    Chess

---
+++ src/main/java/com/vimemacs/reuse/Chess.java
@@ -0,0 +1,36 @@
+package com.vimemacs.reuse;
+
+/**
+ * Inheritance, constructors and arguments
+ * @author HWD
+ * @date 2023/8/7 14:30
+ */
+
+class Game {
+    Game(int i) {
+        System.out.println("Game constructor");
+    }
+}
+
+class BoardGame extends Game {
+    BoardGame(int i) {
+        super(i);
+        System.out.println("BoardGame constructor");
+    }
+}
+
+public class Chess extends BoardGame {
+    Chess() {
+        super(11);
+        System.out.println("Chess constructor");
+    }
+
+    public static void main(String[] args) {
+        Chess x = new Chess();
+    }
+}
+/* Output:
+Game constructor
+BoardGame constructor
+Chess constructor
+*/
