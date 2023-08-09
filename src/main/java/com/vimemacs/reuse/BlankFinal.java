d8aa1aec03dd25050afc35eec88af6cab0f08af3
VimIcewind <VimIcewind@gmail.com>
2023-08-09 14:26:55 +0800

    BlankFinal

---
+++ src/main/java/com/vimemacs/reuse/BlankFinal.java
@@ -0,0 +1,37 @@
+package com.vimemacs.reuse;
+
+/**
+ * "Blank" final fields
+ * @author HWD
+ * @date 2023/8/9 14:18
+ */
+
+class Poppet {
+    private int i;
+
+    Poppet(int ii) {
+        i = ii;
+    }
+}
+
+public class BlankFinal {
+    private final int i = 0; // Initialized final
+    private final int j; // Blank final
+    private final Poppet p; // Blank final reference
+    
+    // Blank finals MUST be initialized in constructor
+    public BlankFinal() {
+        j = 1; // Initialize blank final
+        p = new Poppet(1); // Init blank final reference
+    }
+    
+    public BlankFinal(int x) {
+        j = x; // Initialize blank final
+        p = new Poppet(x); // Init blank final reference
+    }
+
+    public static void main(String[] args) {
+        new BlankFinal();
+        new BlankFinal(47);
+    }
+}
