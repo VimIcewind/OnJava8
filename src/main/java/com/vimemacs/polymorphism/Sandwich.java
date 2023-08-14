59ecae01636d8aae2b1df752317e37035d42d4c6
VimIcewind <VimIcewind@gmail.com>
2023-08-14 15:01:42 +0800

    Sandwich

---
+++ src/main/java/com/vimemacs/polymorphism/Sandwich.java
@@ -0,0 +1,66 @@
+package com.vimemacs.polymorphism;
+
+/**
+ * Order of constructor calls
+ * @author HWD
+ * @date 2023/8/14 14:52
+ */
+
+class Meal {
+    Meal() {
+        System.out.println("Meal()");
+    }
+}
+
+class Bread {
+    Bread() {
+        System.out.println("Bread()");
+    }
+}
+
+class Cheese {
+    Cheese() {
+        System.out.println("Cheese()");
+    }
+}
+
+class Lettuce {
+    Lettuce() {
+        System.out.println("Lettuce()");
+    }
+}
+
+class Lunch extends Meal {
+    Lunch() {
+        System.out.println("Lunch()");
+    }
+}
+
+class PortableLunch extends Lunch {
+    PortableLunch() {
+        System.out.println("PortableLunch()");
+    }
+}
+
+public class Sandwich extends PortableLunch {
+    private Bread b = new Bread();
+    private Cheese c = new Cheese();
+    private Lettuce l = new Lettuce();
+    
+    public Sandwich() {
+        System.out.println("Sandwich()");
+    }
+    
+    public static void main(String[] args) {
+        new Sandwich();
+    }
+}
+/* Output:
+Meal()
+Lunch()
+PortableLunch()
+Bread()
+Cheese()
+Lettuce()
+Sandwich()
+*/
