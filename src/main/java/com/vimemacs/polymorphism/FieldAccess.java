838f97c81839a6127455121f6189de45e56f9e20
VimIcewind <VimIcewind@gmail.com>
2023-08-14 10:46:34 +0800

    FieldAccess

---
+++ src/main/java/com/vimemacs/polymorphism/FieldAccess.java
@@ -0,0 +1,42 @@
+package com.vimemacs.polymorphism;
+
+/**
+ * Direct field access is determined at compile time
+ * @author HWD
+ * @date 2023/8/14 10:36
+ */
+
+class Super {
+    public int field = 0;
+    
+    public int getField() {
+        return field;
+    }
+}
+
+class Sub extends Super {
+    public int field = 1;
+
+    @Override
+    public int getField() {
+        return field;
+    }
+    
+    public int getSuperField() {
+        return super.field;
+    }
+}
+
+public class FieldAccess {
+    public static void main(String[] args) {
+        Super sup = new Sub(); // Upcast
+        System.out.println("sup.field = " + sup.field + ", sup.getField() = " + sup.getField());
+        Sub sub = new Sub();
+        System.out.println("sub.field = " + sub.field + ", sub.getField() = " + sub.getField()
+                            + ", sub.getSuperField() = " + sub.getSuperField());
+    }
+}
+/* Output:
+sup.field = 0, sup.getField() = 1
+sub.field = 1, sub.getField() = 1, sub.getSuperField() = 0
+*/
