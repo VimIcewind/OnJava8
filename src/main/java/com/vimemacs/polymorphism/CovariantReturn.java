890c70bfbac5e7d471ef2409736e89fa8160de2b
VimIcewind <VimIcewind@gmail.com>
2023-08-15 11:12:32 +0800

    CovariantReturn

---
+++ src/main/java/com/vimemacs/polymorphism/CovariantReturn.java
@@ -0,0 +1,48 @@
+package com.vimemacs.polymorphism;
+
+/**
+ * @author HWD
+ * @date 2023/8/15 11:00
+ */
+
+class Grain {
+    @Override
+    public String toString() {
+        return "Grain";
+    }
+}
+
+class Wheat extends Grain {
+    @Override
+    public String toString() {
+        return "Wheat";
+    }
+}
+
+class Mill {
+    Grain process() {
+        return new Grain();
+    }
+}
+
+class WheatMill extends Mill {
+    @Override
+    Wheat process() {
+        return new Wheat();
+    }
+}
+
+public class CovariantReturn {
+    public static void main(String[] args) {
+        Mill m = new Mill();
+        Grain g = m.process();
+        System.out.println(g);
+        m = new WheatMill();
+        g = m.process();
+        System.out.println(g);
+    }
+}
+/* Output:
+Grain
+Wheat
+*/
