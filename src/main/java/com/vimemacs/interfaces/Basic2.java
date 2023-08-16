49ea4e4640797cb62a8609cc5ad6cea329e289c2
VimIcewind <VimIcewind@gmail.com>
2023-08-16 10:22:09 +0800

    Basic2

---
+++ src/main/java/com/vimemacs/interfaces/Basic2.java
@@ -0,0 +1,15 @@
+package com.vimemacs.interfaces;
+
+/**
+ * @author HWD
+ * @date 2023/8/15 16:35
+ */
+abstract class Basic2 extends Basic {
+    int f() {
+        return 111;
+    }
+    
+    abstract void g();
+    
+    // unimplemented() still not implemented
+}
