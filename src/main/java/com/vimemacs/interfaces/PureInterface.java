8f303790772eda9e7789f2e5a5a3444e227f70cf
VimIcewind <VimIcewind@gmail.com>
2023-08-16 11:02:10 +0800

    PureInterface

---
+++ src/main/java/com/vimemacs/interfaces/PureInterface.java
@@ -0,0 +1,12 @@
+package com.vimemacs.interfaces;
+
+/**
+ * Interface only looked like this before Java 8
+ * @author HWD
+ * @date 2023/8/16 10:57
+ */
+public interface PureInterface {
+    int m1();
+    void m2();
+    double m3();
+}
