5e17a82c30a755bd5c1b3def3d1d146d19388daf
VimIcewind <VimIcewind@gmail.com>
2023-08-16 10:18:27 +0800

    AttemptToUseBasic

---
+++ src/main/java/com/vimemacs/interfaces/AttemptToUseBasic.java
@@ -0,0 +1,10 @@
+package com.vimemacs.interfaces;
+
+/**
+ * @author HWD
+ * @date 2023/8/15 16:34
+ */
+public class AttemptToUseBasic {
+    //! Basic b = new Basic();
+    // error: Basic is abstract; cannot be instantiated
+}
