f605e2849ff8e75424002c61f32eaa05276a5f58
VimIcewind <VimIcewind@gmail.com>
2023-08-18 11:02:13 +0800

    Months

---
+++ src/main/java/com/vimemacs/interfaces/Months.java
@@ -0,0 +1,13 @@
+package com.vimemacs.interfaces;
+
+/**
+ * Using interfaces to create groups of constants
+ * @author HWD
+ * @date 2023/8/18 10:59
+ */
+public interface Months {
+    int JANUARY = 1, FEBRUARY = 2, MARCH = 3,
+    APRIL = 4, MAY = 5, JUNE = 6, JULY = 7,
+    AUGUST = 8, SEPTEMBER = 9, OCTOBER = 10,
+    NOVEMBER = 11, DECEMBER = 12;
+}
